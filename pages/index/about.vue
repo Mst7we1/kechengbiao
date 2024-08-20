d7cd9e5f8308067fd82c3c685b4e57b4d8efc5ce
Mst7we1 <msdwei0114@163.com>
2024-08-20 22:52:06 +0800

    Add files via upload

---
+++ pages/index/about.vue
@@ -0,0 +1,155 @@
+<template>
+  <view class="about-page">
+    <view class="header">
+      <image class="logo" :src="logoUrl" mode="aspectFit"></image>
+      <text class="title">关于此软件</text>
+    </view>
+    <view class="content">
+      <text class="description">这是一个及其简约的课程表软件，基于uni-app框架开发。</text>
+      <view class="author-info">
+        <view class="info-row">
+          <text class="author-name">作者: 张鹏宇</text>
+        </view>
+        <view class="info-row">
+          <text class="author-name">部分数据支持: 张敬伟</text>
+        </view>
+        <view class="info-row">
+          <text class="author-email">反馈邮箱: wwei50057@gmail.com</text>
+        </view>
+        <view class="info-row">
+          <text class="author-github">GitHub: <a :href="githubUrl" target="_blank">Mst7we1</a></text>
+        </view>
+      </view>
+    </view>
+    <component-global-tab-bar></component-global-tab-bar>
+  </view>
+</template>
+
+<script>
+import ComponentGlobalTabBar from '@/components/TabBar.vue';
+
+export default {
+  components: {
+    ComponentGlobalTabBar
+  },
+  data() {
+    return {
+      logoUrl: '../../static/logo.png',
+      githubUrl: 'https://github.com/Mst7we1'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.about-page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+  padding: 20rpx;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20rpx;
+  padding: 10rpx 0;
+  border-bottom: 1rpx solid #ccc;
+}
+
+.logo {
+  width: 80rpx;
+  height: 80rpx;
+  margin-right: 20rpx;
+  border-radius: 50%; /* 圆角 */
+  box-shadow: 0 2rpx 4rpx rgba(0, 0, 0, 0.1); /* 添加阴影 */
+  transition: transform 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.logo:hover {
+  transform: scale(1.1); /* 鼠标悬停时放大图标 */
+}
+
+.title {
+  font-size: 36rpx;
+  font-weight: bold;
+  color: #333;
+}
+
+.content {
+  flex-grow: 1;
+  margin-bottom: 100rpx; /* 为底部导航栏预留空间 */
+}
+
+.description {
+  margin-bottom: 20rpx;
+  font-size: 30rpx;
+  color: #333;
+  line-height: 1.5;
+}
+
+.author-info {
+  margin-top: 20rpx;
+  font-size: 28rpx;
+  color: #333;
+}
+
+.info-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10rpx;
+}
+
+.author-name {
+  font-weight: bold;
+  color: #333;
+}
+
+.author-email {
+  color: #666;
+}
+
+.author-github {
+  color: #333;
+}
+
+.author-github a {
+  color: #333;
+  text-decoration: none;
+}
+
+.author-github a:hover {
+  text-decoration: underline;
+}
+
+/* 底部导航栏 */
+.fixed-tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100rpx; /* 根据实际需要调整高度 */
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: 0 -1px 2px rgba(0, 0, 0, 0.1);
+}
+
+.tab-item {
+  text-align: center;
+  width: 50%;
+}
+
+.tab-item image {
+  width: 50rpx;
+  height: 50rpx;
+  transition: transform 0.3s ease-in-out; /* 添加过渡动画 */
+}
+
+.tab-item:hover image {
+  transform: scale(1.1); /* 鼠标悬停时放大图标 */
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,226 @@
+<template>
+  <view>
+    <SqTimetable
+      :time="time"
+      :image="image"
+      :timeColor="timeColor"
+      :courseList="courseList"
+      :showNotWeek="true"
+      :week="12"
+      @click="getCourse"
+    >
+    </SqTimetable>
+  </view>
+</template>
+
+<script>
+	
+import SqTimetable from "../../components/sq-timetable/sq-timetable.vue";
+export default {
+  components: {
+    SqTimetable,
+  },
+  data() {
+    return {
+      time: [
+        ["8:20", "9:00"],
+        ["9:05", "9:45"],
+        ["10:00", "10:40"],
+        ["10:45", "11:25"],
+        ["11:30", "12:10"],
+        ["13:30", "14:10"],
+        ["14:15", "14:55"],
+        ["15:10", "15:50"],
+        ["15:55", "16:35"],
+        ["16:40", "17:20"],
+        ["18:00", "18:40"],
+        ["18:45", "19:25"],
+        ["19:40", "20:20"],
+        ["20:25", "21:05"],
+      ],
+      image: null,
+      colorArrays: [
+        "#70eb55",
+        "#ffc450",
+        "#ff9bd7",
+        "#86affe",
+        "#58eea4",
+        "#ffa17d",
+        "#1cbbb4",
+        "#6964ad",
+        "#d42245",
+        "#218285",
+      ],
+      timeColor: "#000000",
+      courseList: [
+        {
+          id: 1000,
+          begin: 1,
+          end: 2,
+          week: 1,
+          name: "数据可视化技术与应用",
+          week_begin: 1,
+          week_end: 16,
+          place: "南215",
+          teacher: "张娜",
+        },
+        {
+          id: 1001,
+          begin: 3,
+          end: 4,
+          week: 1,
+          name: "Linux操作系统管理",
+          week_begin: 1,
+          week_end: 16,
+          place: "南415",
+          teacher: "刘晓丹",
+        },
+        {
+          id: 1003,
+          begin: 7,
+          end: 8,
+          week: 1,
+          name: "数据采集与预处理",
+          week_begin: 1,
+          week_end: 16,
+          place: "西306",
+          teacher: "王磊",
+        },
+        {
+          id: 1004,
+          begin: 1,
+          end: 2,
+          week: 2,
+          name: "Scala编程基础",
+          week_begin: 1,
+          week_end: 16,
+          place: "南610",
+          teacher: "赵阳",
+        },
+        {
+          id: 1005,
+          begin: 5,
+          end: 6,
+          week: 2,
+          name: "美育",
+          week_begin: 1,
+          week_end: 16,
+          place: "美育实训室1（小白楼二楼）",
+          teacher: "曹洋",
+        },
+        {
+          id: 1006,
+          begin: 1,
+          end: 2,
+          week: 3,
+          name: "党史",
+          week_begin: 1,
+          week_end: 8,
+          place: "阶梯教室1",
+          teacher: "李岩",
+        },
+        {
+          id: 1007,
+          begin: 5,
+          end: 6,
+          week: 3,
+          name: "面向对象程序设计",
+          week_begin: 1,
+          week_end: 8,
+          place: "南415",
+          teacher: "寇艳艳",
+        },
+        {
+          id: 1008,
+          begin: 3,
+          end: 4,
+          week: 4,
+          name: "数据可视化技术与应用",
+          week_begin: 1,
+          week_end: 16,
+          place: "南215",
+          teacher: "张娜",
+        },
+        {
+          id: 1009,
+          begin: 5,
+          end: 6,
+          week: 4,
+          name: "WEB前端设计(js)",
+          week_begin: 1,
+          week_end: 16,
+          place: "西405",
+          teacher: "杨秋叶",
+        },
+        {
+          id: 1010,
+          begin: 9,
+          end: 10,
+          week: 4,
+          name: "形势与政策",
+          week_begin: 1,
+          week_end: 4,
+          place: "阶梯教室1",
+          teacher: "王霄云",
+        },
+        {
+          id: 1011,
+          begin: 9,
+          end: 10,
+          week: 4,
+          name: "形势与政策",
+          week_begin: 1,
+          week_end: 4,
+          place: "阶梯教室1",
+          teacher: "王霄云",
+        },
+        {
+          id: 1012,
+          begin: 1,
+          end: 2,
+          week: 5,
+          name: "面向对象程序设计",
+          week_begin: 1,
+          week_end: 8,
+          place: "南602",
+          teacher: "寇艳艳",
+        },
+        {
+          id: 1013,
+          begin: 3,
+          end: 4,
+          week: 5,
+          name: "数据采集与预处理",
+          week_begin: 1,
+          week_end: 16,
+          place: "西306",
+          teacher: "王磊",
+        },
+        {
+          id: 1014,
+          begin: 5,
+          end: 6,
+          week: 5,
+          name: "WEB前端设计(js)",
+          week_begin: 1,
+          week_end: 16,
+          place: "南215",
+          teacher: "杨秋叶",
+        },
+      ],
+    };
+  },
+  onLoad() {},
+  methods: {
+    getCourse(item) {
+      console.log(item);
+    },
+  },
+};
+
+
+</script>
+
+
+
+<style></style>
